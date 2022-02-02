6dd1a7d4499d916dfb651f8a1c26c51a719036e3
Alen Bijelic <alen.bijelic@heig-vd.ch>
2022-02-02 12:45:33 +0100

    Change Node version and create uuid for musician

--- docker/image-musician/src/app.js
+++ docker/image-musician/src/app.js
@@ -1,3 +1,6 @@
+const { v4: uuidv4 } = require('uuid')
+
+// Map of instruments and their sound
 const instrumentSound = new Map([
    ['piano', 'ti-ta-ti'],
    ['trumpet', 'pouet'],
@@ -6,15 +9,19 @@ const instrumentSound = new Map([
    ['drum', 'boum-boum'],
 ]);
 
-console.log(process.argv[2]);
-
+// Retrieve instrument passed in argument
 const instrument = process.argv[2];
 
+// Retrieve sound of the instrument from the map
 const musicianSound = instrumentSound.get(instrument) || null;
 
-console.log(musicianSound);
-
+// Check if the instrument and the sound exists in the map
 if(musicianSound == null){
    console.log("ERROR: No such instrument");
    process.exit(1);
 }
+
+// Create uuid for the musician
+const musician_uuid = uuidv4()
+
+console.log('UUID : ' + musician_uuid);
